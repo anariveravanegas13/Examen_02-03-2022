7ec1b4f8954629b453ac10eef5c5487cf22962a2
anariveravanegas13 <100798776+anariveravanegas13@users.noreply.github.com>
2022-03-02 16:35:49 -0500

    Add files via upload

---
+++ EXAMEN/script/script.js
@@ -0,0 +1,15 @@
+let salario
+let ch
+const hora = 20000
+const hExtra = 30000
+
+
+function horasT(params) {
+    hora = number(prompt('Ingrese la cantidad de las horas trabajadas'))
+}
+horasT()
+
+function calcular(salario) {
+    else if (horasT >=1 && horasT <=40) {
+        salario = cantHora *horas
+}
